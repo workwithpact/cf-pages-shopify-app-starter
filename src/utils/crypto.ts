0771e67a3cd18c3fdb20830d9006a017d783ea44
Alex Leclair <alex@workwithpact.com>
2022-01-15 17:25:07 -0500

    Encryption & Decryption work!

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -6,9 +6,14 @@ const encrypt = async (algo: 'AES-CBC' | 'AES-CTR' | 'AES-GCM', key: string, dat
     false,
     ["encrypt"],
   )
-  const encryptedData = await crypto.subtle.encrypt({name: algo, iv: crypto.getRandomValues(new Uint8Array(16))}, importedKey, new TextEncoder().encode(data));
+  const iv = crypto.getRandomValues(new Uint8Array(16));
+  const encryptedData = await crypto.subtle.encrypt({name: algo, iv}, importedKey, new TextEncoder().encode(data));
   // @ts-ignore
-  const b64Data = btoa(String.fromCharCode(...new Uint8Array(encryptedData)))
+  const dataArray = new Uint8Array(encryptedData)
+  const finalArray = new Uint8Array(iv.length + dataArray.length)
+  finalArray.set(iv, 0);
+  finalArray.set(dataArray, iv.length);
+  const b64Data = btoa(String.fromCharCode.apply(null, finalArray as any));
   return b64Data;
 }
 
@@ -20,10 +25,12 @@ const decrypt = async (algo: 'AES-CBC' | 'AES-CTR' | 'AES-GCM', key: string, dat
     false,
     ["decrypt"],
   )
-  const decryptedData = await crypto.subtle.decrypt({name: algo, iv: crypto.getRandomValues(new Uint8Array(16))}, importedKey, new TextEncoder().encode(data));
-  // @ts-ignore
-  const b64Data = btoa(String.fromCharCode(...new Uint8Array(decryptedData)))
-  return b64Data;
+  const byteArray = Uint8Array.from(atob(data), c => c.charCodeAt(0))
+  const iv = byteArray.slice(0, 16);
+  const message = byteArray.slice(16, byteArray.length);
+  const decryptedData = await crypto.subtle.decrypt({name: algo, iv}, importedKey, message);
+  const decryptedMessage = new TextDecoder().decode(decryptedData)
+  return decryptedMessage
 }
 
 export { encrypt, decrypt }
